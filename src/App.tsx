155269ec80a491ee0defa3518e9a1f3cade5e701
Achu1ahm <asfaqahmed128@gmail.com>
2025-02-08 11:10:26 +0530

    base setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import About from './About';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div>        
         {/* Define the routes */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+        <Router>
+          <Routes>
+            {/* Public Route */}
+            <Route path='/login' element={<Login />} />
+
+            {/* Protected Routes */}
+            <Route path="/doctor" element={<ProtectedRoute>
+              <UserChat />
+            </ProtectedRoute>} />
+
+            {/* Default Route */}
+            <Route path="/" element={<Navigate to="/login" replace />} ></Route>
+
+            {/* Catch all route */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+          </Router>
       </div>
     </Router>
   );
